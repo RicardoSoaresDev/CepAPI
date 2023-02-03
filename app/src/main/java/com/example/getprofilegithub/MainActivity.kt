afcf9251952790df828643c7c0b59e0e6ee6aad4
Ricardo Ribeiro <ricardoribeiro@Macbook-de-Ricardo.local>
2023-02-03 09:53:24 -0300

    API cep funcionando

--- app/src/main/java/com/example/getprofilegithub/MainActivity.kt
+++ app/src/main/java/com/example/getprofilegithub/MainActivity.kt
@@ -17,23 +17,21 @@ import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
 
-    private var address: EditText? = null
-    private var neighbourhood: EditText? = null
-    private var city: EditText? = null
-    private var state: EditText? = null
+    private lateinit var address: EditText
+    private lateinit var neighbourhood: EditText
+    private lateinit var city: EditText
+    private lateinit var state: EditText
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val cep = findViewById<EditText>(R.id.cep)
-
-//        val address = findViewById<TextView>(R.id.address)
-//        val neighbourhood = findViewById<EditText>(R.id.neighbourhood)
-//        val city = findViewById<EditText>(R.id.city)
-//        val state = findViewById<EditText>(R.id.state)
-
-        val button = findViewById<Button>(R.id.button1)
+        address = findViewById(R.id.address)
+        neighbourhood = findViewById(R.id.neighbourhood)
+        city = findViewById(R.id.city)
+        state = findViewById(R.id.state)
+        button = findViewById(R.id.button1)
 
         button.setOnClickListener {
 
@@ -42,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun getData(address: EditText?, neighbourhood: EditText?, city: EditText?, state: EditText?) {
+    private fun getData(address: EditText, neighbourhood: EditText, city: EditText, state: EditText) {
 
         val retrofitBase = NetworkUtils.getRetrofitInstance()
         val endPointPath = retrofitBase.create(EndPointPath::class.java)
@@ -52,26 +50,17 @@ class MainActivity : AppCompatActivity() {
         callback.enqueue(object: Callback<Model> {
             override fun onResponse(call: Call<Model>, response: Response<Model>) {
 
-                address?.setText(response.body()?.address)
-                neighbourhood?.setText(response.body()?.neighbourhood)
-                city?.setText(response.body()?.city)
-                state?.setText(response.body()?.state)
+                address.setText(response.body()?.address).toString()
+                neighbourhood.setText(response.body()?.neighbourhood).toString()
+                city.setText(response.body()?.city).toString()
+                state.setText(response.body()?.state).toString()
 
-                Toast.makeText(this@MainActivity, "Ok", Toast.LENGTH_SHORT).show()
+//                Toast.makeText(this@MainActivity, "Ok", Toast.LENGTH_SHORT).show()
             }
 
             override fun onFailure(call: Call<Model>, t: Throwable) {
                 Toast.makeText(this@MainActivity, "Erro", Toast.LENGTH_SHORT).show()
-
-                println("$t, >>>>>>>>>>>>>")
             }
         })
     }
 }
-
-
-
-//                address.text = response.body()?.Address
-//                neighbourhood.text = response.body()?.neighbourhood
-//                city.text = response.body()?.city
-//                state.text = response.body()?.state
